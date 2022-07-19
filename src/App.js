4da498a0e692707c6b02544730ef691ef85b0819
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-19 23:37:45 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+
+      <Header></Header>
 
       
       
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Header = () => {
+
+
+    const manu = <>
+     <li><a>Add A Product</a></li>
+    </>
+
+    return (
+    
+        <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            {manu}
+              
+              
+            </ul>
+          </div>
+          <a class="btn btn-ghost normal-case text-xl">POS Zero</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">
+           
+            {manu}
+           
+          </ul>
+        </div>
+
+        
+        <div class="navbar-end">
+        <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered" />
+    </div>
+        <div class="dropdown dropdown-end">
+      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" />
+        </div>
+      </label>
+      <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+        </div>
+      </div>
+    );
+};
+
+export default Header;
