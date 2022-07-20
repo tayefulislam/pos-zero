12e2464a45fc0c0abb21209ee7086522f2cae6c3
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-20 09:02:14 +0600

    Item container design complete

--- src/Pages/POS_Modules/Item/Item.js
+++ src/Pages/POS_Modules/Item/Item.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-const Item = () => {
+const Item = ({item,handleAddToCart}) => {
 
-    
+
+    console.log(item)
+
+    const {_id:id,itemName,unitPrice,vat}=item
+
     return (
-        <div>
-
-
-            
-        </div>
+        <div onClick={()=>handleAddToCart(item)} class="card w-full bg-base-100 shadow-xl">
+  <figure class="p-2">
+    <img src={item?.image} alt="Shoes" class="rounded-xl w-20" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">{itemName}</h2>
+    <p>Unit Price :{unitPrice} </p>
+    <p>VAT :{vat} % </p>
+    
+  </div>
+</div>
     );
 };
 
--- src/Pages/POS_Modules/POS/POS.js
+++ src/Pages/POS_Modules/POS/POS.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import './POS.css';
+import Item from '../Item/Item';
+import { toast } from 'react-toastify';
 
 const POS = () => {
 
@@ -12,16 +14,51 @@ const POS = () => {
     )
   )
 
-  console.log(data)
+//   console.log(data)
+
+
+
+const [carts,setCart]=useState([])
+
+
+//handle to addtocart
+
+
+
+const handleAddToCart=(item)=>{
+
+    const itemCheck=carts.find(cart =>cart._id === item._id)
+if(!itemCheck){
+    carts.push(item)
+    console.log(carts)
+}
+
+else{
+    toast.error("Item Already In Cart")
+}
+   
+
+
+}
+
+
+
+
 
     return (
         <div>
 
             <div className='pos-container'>
 
-                <div className='grid grid-cols-4'>
+                <div className='grid grid-cols-4 gap-1'>
 
-                    
+                    {
+                        data?.map(item =><Item
+                        key={item._id}
+                        item={item}
+                        handleAddToCart={handleAddToCart}
+                        ></Item>)
+                    }
 
 
 
